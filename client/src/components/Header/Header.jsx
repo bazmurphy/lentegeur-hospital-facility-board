ae7e846d61b6c16bbeb7f70735965fbf9fadebea
Baz Murphy <bazmurphy@gmail.com>
2023-06-18 23:27:53 +0100

    added Header with basic nav to test routing

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,40 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+			<h2>Header</h2>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+					<li>
+						<Link to="/services">Services</Link>
+					</li>
+					<li>
+						<Link to="/news">News</Link>
+					</li>
+					<li>
+						<Link to="/events">Events</Link>
+					</li>
+					<li>
+						<Link to="/gallery">Gallery</Link>
+					</li>
+					<li>
+						<Link to="/get-involved">Get Involved</Link>
+					</li>
+					<li>
+						<Link to="/contact-us">Contact Us</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
