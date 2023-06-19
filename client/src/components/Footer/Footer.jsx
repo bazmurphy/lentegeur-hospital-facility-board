2b650a78d8a684b8533e1480c9ac645fc9024c74
KhadijaHaji <108950237+KhadijaHaji@users.noreply.github.com>
2023-06-19 17:56:20 +0100

    Add address to Footer component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,31 @@
 import "./Footer.css";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-	return (
-		<footer>
-			<h2>Footer</h2>
-		</footer>
-	);
+  return (
+    <footer>
+		 <div className="footer-container">
+			<div className="address">
+          <h3>ADDRESS</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing</p>
+        </div>
+        <div className="contact-details">
+          <h3>CONTACTS</h3>
+          <ul>
+            <li>
+              <a href="tel:+8201453434343">8 201 45 343 4343</a>
+            </li>
+            <li>
+              <a href="tel:+4434334334322">44 343 343 34 322</a>
+            </li>
+            <li>
+              <a href="mailto:info@hospital.com">info@hospital.com</a>
+            </li>
+          </ul>
+        </div>
+		 </div>
+    </footer>
+  );
 };
 
 export default Footer;
