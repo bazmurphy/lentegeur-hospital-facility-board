a94f6f6c7e837770d8ad40587ab0117eec61d8d4
Baz Murphy <bazmurphy@gmail.com>
2023-06-20 17:43:02 +0100

    added GetInvolvedPage

--- client/src/pages/GetInvolvedPage/GetInvolvedPage.jsx
+++ client/src/pages/GetInvolvedPage/GetInvolvedPage.jsx
@@ -1,10 +1,23 @@
 import "./GetInvolvedPage.css";
+import Donate from "./components/Donate/Donate";
+import Volunteer from "./components/Volunteer/Volunteer";
 
 const GetInvolvedPage = () => {
 	return (
-		<>
-			<h1>Get Involved Page</h1>
-		</>
+		<div className="get-involved-page">
+			<section className="get-involved-section">
+				<h1 className="get-involved-heading">Get Involved</h1>
+				<div className="line-divider"></div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+					ratione consequatur inventore aliquam atque iste aperiam itaque
+					voluptatum at officiis ipsam facilis, nam eaque quisquam quia et
+					sapiente quae minima!
+				</p>
+			</section>
+			<Donate />
+			<Volunteer />
+		</div>
 	);
 };
 
