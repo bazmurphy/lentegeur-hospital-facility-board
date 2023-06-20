22f585cb56b1e709b3f1cefb760e51c7d692b379
MacOS <macos@blessing.local>
2023-06-20 22:21:06 +0100

    feat: widescreen nav

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,37 +4,58 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
 	return (
 		<header>
-			<h2>Header</h2>
 			<nav>
-				<ul>
+				<div className="logo-name">
+					<img
+						className="logo"
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Circle_Davys-Grey_Solid.svg/1200px-Circle_Davys-Grey_Solid.svg.png"
+					></img>
+					<div className="name">Lentegeur Hospital</div>
+				</div>
+				<ul className="navigation-links">
 					<li>
-						<NavLink to="/">Home</NavLink>
+						<NavLink to="/" className="text-link">
+							HOME
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/about">About</NavLink>
+						<NavLink to="/about" className="text-link">
+							ABOUT LSH
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/services">Services</NavLink>
+						<NavLink to="/services" className="text-link">
+							SERVICES
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/news">News</NavLink>
+						<NavLink to="/news" className="text-link">
+							NEWS
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/events">Events</NavLink>
+						<NavLink to="/events" className="text-link">
+							EVENTS
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/gallery">Gallery</NavLink>
+						<NavLink to="/gallery" className="text-link">
+							GALLERY
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/get-involved">Get Involved</NavLink>
+						<NavLink to="/get-involved" className="text-link">
+							DONATE
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/contact-us">Contact Us</NavLink>
+						<NavLink to="/contact-us" className="text-link">
+							CONTACT US
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
 		</header>
 	);
 };
-
 export default Header;
