6f599c1680951b1e9cbb5f017d9d686d4e6b2f27
Baz Murphy <bazmurphy@gmail.com>
2023-06-20 17:44:26 +0100

    added Volunteer Component

---
+++ client/src/pages/GetInvolvedPage/components/Volunteer/Volunteer.jsx
@@ -0,0 +1,30 @@
+import "./Volunteer.css";
+import VolunteerForm from "./VolunteerForm/VolunteerForm";
+
+const Volunteer = () => {
+	return (
+		<section className="volunteer-section">
+			<h2>Volunteer</h2>
+			<div className="line-divider"></div>
+			<div className="volunteer-image-container">
+				<img
+					className="volunteer-image"
+					src="https://wcva.cymru/wp-content/uploads/2021/05/shutterstock_1215144616-1.jpg"
+					alt="a person volunteering"
+				/>
+			</div>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+				ipsam pariatur sapiente nam assumenda adipisci architecto eius
+				repellendus voluptas, quidem earum, quo in incidunt. Obcaecati quibusdam
+				asperiores in quod atque ipsam sapiente, fugiat dolore impedit
+				perferendis ipsa animi soluta assumenda laudantium et adipisci
+				voluptatibus doloribus dolorum mollitia reiciendis illum consequatur. Et
+				dolor reiciendis laudantium, modi architecto vel tenetur rem ducimus.
+			</p>
+			<VolunteerForm />
+		</section>
+	);
+};
+
+export default Volunteer;
