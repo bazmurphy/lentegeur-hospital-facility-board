e06ba49215bb787ef30047f75cc05dc18e13a6eb
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-06-20 11:34:16 +0100

    add example json for services

--- client/src/pages/ServicesPage/ServicesPage.jsx
+++ client/src/pages/ServicesPage/ServicesPage.jsx
@@ -1,9 +1,36 @@
 import "./ServicesPage.css";
+import Services from "./components/Services";
+
+const servicesData = [
+	{
+		title: "Emergency Services",
+		image: "https://example.com/emergency.jpg",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		title: "Surgical Procedures",
+		image: "https://example.com/surgery.jpg",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		title: "Diagnostic Imaging",
+		image: "https://example.com/imaging.jpg",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+];
 
 const ServicesPage = () => {
 	return (
 		<>
-			<h1>Services Page</h1>
+			<h1>Services</h1>
+			{servicesData.map((service, index) => (
+				<Services
+					key={index}
+					title={service.title}
+					image={service.image}
+					description={service.description}
+				/>
+			))}
 		</>
 	);
 };
