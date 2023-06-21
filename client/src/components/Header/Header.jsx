7b34b425bf7e5998f9a038a26642f7225666b57e
MacOS <macos@blessing.local>
2023-06-21 02:32:49 +0100

    feat: added hamburger menu

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -15,30 +15,30 @@ const Header = () => {
 		<header>
 			<h2>Lentegeur Hospital</h2>
 			<nav ref = {navRef}>
-				<ul>
-					<li>
-						<NavLink to="/">Home</NavLink>
+				<ul className="ul-container">
+					<li >
+						<NavLink to="/" className="navlink-text">Home</NavLink>
 					</li>
 					<li>
-						<NavLink to="/about">About</NavLink>
+						<NavLink to="/about" className="navlink-text">About</NavLink>
 					</li>
 					<li>
-						<NavLink to="/services">Services</NavLink>
+						<NavLink to="/services" className="navlink-text">Services</NavLink>
 					</li>
 					<li>
-						<NavLink to="/news">News</NavLink>
+						<NavLink to="/news" className="navlink-text">News</NavLink>
 					</li>
 					<li>
-						<NavLink to="/events">Events</NavLink>
+						<NavLink to="/events" className="navlink-text">Events</NavLink>
 					</li>
 					<li>
-						<NavLink to="/gallery">Gallery</NavLink>
+						<NavLink to="/gallery" className="navlink-text">Gallery</NavLink>
 					</li>
 					<li>
-						<NavLink to="/get-involved">Get Involved</NavLink>
+						<NavLink to="/get-involved" className="navlink-text">Get Involved</NavLink>
 					</li>
 					<li>
-						<NavLink to="/contact-us">Contact Us</NavLink>
+						<NavLink to="/contact-us" className="navlink-text">Contact Us</NavLink>
 					</li>
 					<button className="nav-btn nav-close-btn"  onClick={showNavbar}>
 						<FaTimes />
