b7adc79d51eb7802ba670c0ddb5faffaa9d04dc1
Oleh <Oleh@progressive-id.co.uk>
2023-06-21 09:40:43 +0100

    feat: changing place of button

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,46 +4,61 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import { useRef } from "react";
 
 const Header = () => {
-
 	const navRef = useRef();
 
 	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav")
-	}
+		navRef.current.classList.toggle("responsive_nav");
+	};
 
 	return (
 		<header>
 			<h2>Lentegeur Hospital</h2>
-			<nav ref = {navRef}>
+			<nav ref={navRef}>
 				<ul className="ul-container">
-					<li >
-						<NavLink to="/" className="navlink-text">Home</NavLink>
+					<li>
+						<NavLink to="/" className="navlink-text">
+							Home
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/about" className="navlink-text">About</NavLink>
+						<NavLink to="/about" className="navlink-text">
+							About
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/services" className="navlink-text">Services</NavLink>
+						<NavLink to="/services" className="navlink-text">
+							Services
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/news" className="navlink-text">News</NavLink>
+						<NavLink to="/news" className="navlink-text">
+							News
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/events" className="navlink-text">Events</NavLink>
+						<NavLink to="/events" className="navlink-text">
+							Events
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/gallery" className="navlink-text">Gallery</NavLink>
+						<NavLink to="/gallery" className="navlink-text">
+							Gallery
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/get-involved" className="navlink-text">Get Involved</NavLink>
+						<NavLink to="/get-involved" className="navlink-text">
+							Get Involved
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to="/contact-us" className="navlink-text">Contact Us</NavLink>
+						<NavLink to="/contact-us" className="navlink-text">
+							Contact Us
+						</NavLink>
 					</li>
-					<button className="nav-btn nav-close-btn"  onClick={showNavbar}>
-						<FaTimes />
-					</button>
 				</ul>
+				<button className="nav-btn nav-close-btn" onClick={showNavbar}>
+					<FaTimes />
+				</button>
 			</nav>
 			<button className="nav-btn" onClick={showNavbar}>
 				<FaBars />
