f78498820838901cc0caa6a25bf0586bcd425094
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-06-23 08:25:14 +0100

    Changing Header.jsx and Header.css
    Adding header-subcontainer, closeNavbar to every Navlink, arrow to every desktop active Navlink, changing design, working on responsivness

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { AiFillCaretUp } from "react-icons/ai";
 import { useRef } from "react";
 
 const Header = () => {
@@ -10,59 +11,112 @@ const Header = () => {
 		navRef.current.classList.toggle("responsive_nav");
 	};
 
+	const closeNavbar = () => {
+		// Check if the window width is less than or equal to 767px (indicating the mobile view)
+		if (window.innerWidth <= 767) {
+			navRef.current.classList.remove("responsive_nav");
+		}
+	};
+
 	return (
 		<header>
-			<h2>Lentegeur Hospital</h2>
-			<nav ref={navRef}>
-				<ul className="ul-container">
-					<li>
-						<NavLink to="/" className="navlink-text">
-							Home
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/about" className="navlink-text">
-							About
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/services" className="navlink-text">
-							Services
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/news" className="navlink-text">
-							News
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/events" className="navlink-text">
-							Events
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/gallery" className="navlink-text">
-							Gallery
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/get-involved" className="navlink-text">
-							Get Involved
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/contact-us" className="navlink-text">
-							Contact Us
-						</NavLink>
-					</li>
-				</ul>
-				<button className="nav-btn nav-close-btn" onClick={showNavbar}>
-					<FaTimes />
+			<div className="header-subcontainer">
+				<div className="header-logo-container">
+					<div className="header-logo-image-container">
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Circle_Davys-Grey_Solid.svg"
+							alt="logo"
+						/>
+					</div>
+					<h2 className="header-logo-title">
+						Lentegeur Hospital
+						<br />
+						Facility Board
+					</h2>
+				</div>
+				<nav ref={navRef}>
+					<ul>
+						<li>
+							<NavLink to="/" className="navlink" onClick={closeNavbar}>
+								Home
+							</NavLink>
+							<div className="navlink-image-container">
+								<AiFillCaretUp />
+							</div>
+						</li>
+						<li>
+							<NavLink to="/about" className="navlink" onClick={closeNavbar}>
+								About LSH
+							</NavLink>
+							<div className="navlink-image-container">
+								<AiFillCaretUp />
+							</div>
+						</li>
+						<li>
+							<NavLink to="/services" className="navlink" onClick={closeNavbar}>
+								Services
+							</NavLink>
+							<div className="navlink-image-container">
+								<AiFillCaretUp />
+							</div>
+						</li>
+						<li>
+							<NavLink to="/news" className="navlink" onClick={closeNavbar}>
+								News
+							</NavLink>
+							<div className="navlink-image-container">
+								<AiFillCaretUp />
+							</div>
+						</li>
+						<li>
+							<NavLink to="/events" className="navlink" onClick={closeNavbar}>
+								Events
+							</NavLink>
+							<div className="navlink-image-container">
+								<AiFillCaretUp />
+							</div>
+						</li>
+						<li>
+							<NavLink to="/gallery" className="navlink" onClick={closeNavbar}>
+								Gallery
+							</NavLink>
+							<div className="navlink-image-container">
+								<AiFillCaretUp />
+							</div>
+						</li>
+						<li>
+							<NavLink
+								to="/get-involved"
+								className="navlink"
+								onClick={closeNavbar}
+							>
+								Get Involved
+							</NavLink>
+							<div className="navlink-image-container">
+								<AiFillCaretUp />
+							</div>
+						</li>
+						<li>
+							<NavLink
+								to="/contact-us"
+								className="navlink"
+								onClick={closeNavbar}
+							>
+								Contact Us
+							</NavLink>
+							<div className="navlink-image-container">
+								<AiFillCaretUp />
+							</div>
+						</li>
+					</ul>
+					<button className="nav-btn nav-close-btn" onClick={showNavbar}>
+						<FaTimes />
+					</button>
+				</nav>
+				<button className="nav-btn" onClick={showNavbar}>
+					<FaBars />
 				</button>
-			</nav>
-			<button className="nav-btn" onClick={showNavbar}>
-				<FaBars />
-			</button>
+			</div>
 		</header>
 	);
 };
