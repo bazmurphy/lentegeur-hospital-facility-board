770b2cb503a9bda6d7a64218fe15c6c9bd806608
KhadijaHaji <108950237+KhadijaHaji@users.noreply.github.com>
2023-06-24 21:22:32 +0100

    carousel updated
    we installed react slick/carousel.
    Updated component and styled it.

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,36 +1,34 @@
 import "./HomePage.css";
-import HomeCarousel from "./components/Carousel/Carousel";
+import Carousel from "./components/Carousel/Carousel";
 import NewsSection from "./components/Latest-News/NewsSection/NewsSection";
 import EventsSection from "./components/Latest-Events/EventsSection";
 import GetInvolved from "./components/GetInvolved/GetInvolvedSection";
 
 const HomePage = () => {
-    return (
-    <div className="container">
-      <div className="hero">
-        <div className="image-carousel">
-          <HomeCarousel />
-        </div>
-      </div>
-      <div className="section">
-        <h2>Latest News</h2>
-        <div className="card-container">
-          <NewsSection />
-        </div>
-      </div>
-      <div className="section">
-        <h2>Latest Events</h2>
-        <div className="card-container">
-          <EventsSection />
-        </div>
-      </div>
-      <div className="section">
-        <div className="icon-container">
-          <GetInvolved />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<section>
+				<Carousel />
+			</section>
+			<section>
+				<h2>Latest News</h2>
+				<div className="card-container">
+					<NewsSection />
+				</div>
+			</section>
+			<section>
+				<h2>Latest Events</h2>
+				<div className="card-container">
+					<EventsSection />
+				</div>
+			</section>
+			<section>
+				<div className="icon-container">
+					<GetInvolved />
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default HomePage;
--- client/src/pages/HomePage/components/Carousel/Carousel.jsx
+++ client/src/pages/HomePage/components/Carousel/Carousel.jsx
@@ -1,24 +1,50 @@
-import { Carousel } from "react-responsive-carousel";
+// import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import image from "../Assets/grey_image.svg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Carousel.css";
 
-const HomeCarousel = () => {
-  return (
-    <Carousel>
-      <div>
-        <img src={image} alt="grey wireframe" />
-        <p className="legend">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, quos.</p>
-      </div>
-      <div>
-        <img src="image2.jpg" alt="hospital" />
-        <p className="legend">Image 2</p>
-      </div>
-      <div>
-        <img src="image3.jpg" alt="doctor" />
-        <p className="legend">Image 3</p>
-      </div>
-    </Carousel>
-  );
+const Carousel = () => {
+	const settings = {
+		dots: true, // Display dots at the bottom indicating image count
+		arrows: true, // Display navigation arrows
+		autoplay: true, // Automatically transition to the next image
+		autoplaySpeed: 3000, // Delay between image transitions (in milliseconds)
+	};
+
+	return (
+		<Slider {...settings}>
+			<div className="slide-container">
+				<h2 className="slide-title">
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry.
+				</h2>
+				<div className="slide-image-container">
+					<img
+						src="https://infoguidenigeria.com/wp-content/uploads/2020/01/african-american-black-doctor-man-626x405.jpg"
+						alt="one"
+					/>
+				</div>
+			</div>
+			<div className="slide-container">
+				<div className="slide-image-container">
+					<img
+						src="https://t4.ftcdn.net/jpg/03/17/85/49/360_F_317854905_2idSdvi2ds3yejmk8mhvxYr1OpdVTrSM.jpg"
+						alt="two"
+					/>
+				</div>
+			</div>
+			<div className="slide-container">
+				<div className="slide-image-container">
+					<img
+						src="https://img.freepik.com/free-photo/woman-doctor-wearing-lab-coat-with-stethoscope-isolated_1303-29791.jpg"
+						alt="three"
+					/>
+				</div>
+			</div>
+		</Slider>
+	);
 };
 
-export default HomeCarousel;
+export default Carousel;
