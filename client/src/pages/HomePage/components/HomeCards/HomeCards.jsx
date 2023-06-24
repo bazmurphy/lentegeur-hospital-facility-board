3402db6a750c662a99c6b2928b8c31c5ed1c3fa1
KhadijaHaji <108950237+KhadijaHaji@users.noreply.github.com>
2023-06-24 22:23:40 +0100

    created HomeCards component

---
+++ client/src/pages/HomePage/components/HomeCards/HomeCards.jsx
@@ -0,0 +1,50 @@
+import "./HomeCards.css";
+import NewsEventsContentBox from "../NewsEventsContentBox/NewsEventsContentBox";
+import Line from "../../../../components/Line/Line";
+
+const newsData = [
+	{
+		title: "Emergency Services",
+		image:
+			"https://clevelandcliniclondon.uk/-/scassets/images/org/locations/london/hospital-services/hospital-services-feature.jpg",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		title: "Surgical Procedures",
+		image:
+			"https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/09/Female_Doctor_Daughter_Mother_1296x728-header-1296x729.jpg?w=1155&h=2268",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		title: "Diagnostic Imaging",
+		image:
+			"https://s3-prod.modernhealthcare.com/s3fs-public/SPONSORED_170619878_AR_-1_RXMUPRMWBUGI.jpg",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+];
+
+const HomeCards = ({ title }) => {
+	return (
+		<section>
+			<h2>{title}</h2>
+			<Line />
+			<div className="cards-list-container">
+				{newsData.map((item, index) => {
+					return (
+						<NewsEventsContentBox
+							key={index}
+							title={item.title}
+							image={item.image}
+							content={item.description}
+						/>
+					);
+				})}
+			</div>
+		</section>
+	);
+};
+
+export default HomeCards;
