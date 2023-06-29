6ff52ea70c737a8f182b5addaf7d761c4956148d
KhadijaHaji <108950237+KhadijaHaji@users.noreply.github.com>
2023-06-29 13:58:28 +0100

    changes to get involved section
    Co-Authored-By: Baz <61154071+bazmurphy@users.noreply.github.com>
    Co-Authored-By: Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>

---
+++ client/src/pages/HomePage/components/HomeGetInvolvedSection/HomeGetInvolvedSection.jsx
@@ -0,0 +1,30 @@
+import "./HomeGetInvolvedSection.css";
+import { Link } from "react-router-dom";
+import Line from "../../../../components/Line/Line";
+
+import { FaHandHoldingMedical, FaPerson } from "react-icons/fa6";
+
+const HomeGetInvolvedSection = () => {
+	return (
+		<section className="home-get-involved-section">
+			<h2>Get Involved</h2>
+			<Line />
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting
+				industry. Lorem Ipsum has been the industrys standard...
+			</p>
+			<div className="home-get-involved-links-container">
+				<Link to="/get-involved" className="home-get-involved-link">
+					<FaHandHoldingMedical className="home-get-involved-link-icon" />
+					<span className="home-get-involved-link-title">Fundraising</span>
+				</Link>
+				<Link to="/get-involved" className="home-get-involved-link">
+					<FaPerson className="home-get-involved-link-icon" />
+					<span className="home-get-involved-link-title">Volunteer</span>
+				</Link>
+			</div>
+		</section>
+	);
+};
+
+export default HomeGetInvolvedSection;
