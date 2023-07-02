d0b30ff4bacf83ce3adf5785390f87850e4e9c53
Baz Murphy <bazmurphy@gmail.com>
2023-07-02 18:48:50 +0100

    merge commit: latest development branch

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,38 +1,108 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { AiFillCaretUp } from "react-icons/ai";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+	const [isNavigationOpen, setIsNavigationOpen] = useState(false);
+	//for closing animation
+	const [isNavigationClosing, setIsNavigationClosing] = useState(false);
+
+	const navLinks = [
+		{ id: 1, to: "/", text: "Home" },
+		{ id: 2, to: "/about", text: "About LSH" },
+		{ id: 3, to: "/services", text: "Services" },
+		{ id: 4, to: "/news", text: "News" },
+		{ id: 5, to: "/events", text: "Events" },
+		{ id: 6, to: "/gallery", text: "Gallery" },
+		{ id: 7, to: "/get-involved", text: "Get Involved" },
+		{ id: 8, to: "/contact-us", text: "Contact Us" },
+	];
+
+	const showNavbar = () => {
+		setIsNavigationOpen(true);
+		document.body.classList.add("disable-scroll");
+	};
+
+	const closeNavbar = () => {
+		document.body.classList.remove("disable-scroll");
+		setIsNavigationOpen(false);
+		setIsNavigationClosing(true);
+
+		setTimeout(() => {
+			setIsNavigationClosing(false);
+		}, 1000); // Wait for the animation to complete before removing the classes
+	};
+
+	// This useEffect hook adds a resize event listener to the window object
+	// to handle changes in the window size. The event listener invokes
+	// the handleResize function whenever the window is resized.
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth > 767) {
+				closeNavbar();
+				setIsNavigationClosing(false);
+			}
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
 		<header>
-			<h2>Header</h2>
-			<nav>
-				<ul>
-					<li>
-						<NavLink to="/">Home</NavLink>
-					</li>
-					<li>
-						<NavLink to="/about">About</NavLink>
-					</li>
-					<li>
-						<NavLink to="/services">Services</NavLink>
-					</li>
-					<li>
-						<NavLink to="/news">News</NavLink>
-					</li>
-					<li>
-						<NavLink to="/events">Events</NavLink>
-					</li>
-					<li>
-						<NavLink to="/gallery">Gallery</NavLink>
-					</li>
-					<li>
-						<NavLink to="/get-involved">Get Involved</NavLink>
-					</li>
-					<li>
-						<NavLink to="/contact-us">Contact Us</NavLink>
-					</li>
-				</ul>
-			</nav>
+			<div className="header-subcontainer">
+				<div className="header-logo-container">
+					<div className="header-logo-image-container">
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Circle_Davys-Grey_Solid.svg"
+							alt="logo"
+						/>
+					</div>
+					<h2 className="header-logo-title">
+						Lentegeur Hospital
+						<br />
+						Facility Board
+					</h2>
+				</div>
+				<nav
+					className={`${isNavigationOpen ? "navigation-open" : ""} ${
+						isNavigationClosing ? "navigation-closing" : ""
+					}`}
+				>
+					<ul>
+						{navLinks.map((navLink) => {
+							return (
+								<li key={navLink.id}>
+									<NavLink
+										to={navLink.to}
+										className="navigation-link"
+										onClick={closeNavbar}
+									>
+										{navLink.text}
+									</NavLink>
+									<div className="navigation-link-icon-container">
+										<AiFillCaretUp />
+									</div>
+								</li>
+							);
+						})}
+					</ul>
+					<button
+						className="navigation-menu-button navigation-menu-button-close"
+						onClick={closeNavbar}
+					>
+						<FaTimes />
+					</button>
+				</nav>
+				<button className="navigation-menu-button" onClick={showNavbar}>
+					<FaBars />
+				</button>
+			</div>
 		</header>
 	);
 };
