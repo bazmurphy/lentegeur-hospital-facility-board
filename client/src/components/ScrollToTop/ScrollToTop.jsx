b33777292593c24ce486a2c2c26d14b02e85cffa
KhadijaHaji <108950237+KhadijaHaji@users.noreply.github.com>
2023-07-05 20:51:34 +0100

    scroll to top added to main

---
+++ client/src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,11 @@
+import ScrollToTop from "react-scroll-to-top";
+
+function ScrollToTopButton() {
+	return (
+		<div>
+			<ScrollToTop smooth />
+		</div>
+	);
+}
+
+export default ScrollToTopButton;
