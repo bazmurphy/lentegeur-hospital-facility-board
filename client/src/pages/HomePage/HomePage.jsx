93d1752f7d7875f2b18536763295cd6d56a70226
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-07-11 15:59:47 +0100

    news home page add

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import "./HomePage.css";
 import HomeHero from "./components/HomeHero/HomeHero";
 import HomeEventsSection from "./components/HomeEventsSection/HomeEventsSection";
 import HomeGetInvolvedSection from "./components/HomeGetInvolvedSection/HomeGetInvolvedSection";
+import HomeNewsSection from "./components/HomeNewsSection/HomeNewsSection";
 
 const HomePage = () => {
 	return (
 		<div className="home-page">
 			<HomeHero />
 			<HomeEventsSection />
+			<HomeNewsSection />
 			<HomeGetInvolvedSection />
 		</div>
 	);
