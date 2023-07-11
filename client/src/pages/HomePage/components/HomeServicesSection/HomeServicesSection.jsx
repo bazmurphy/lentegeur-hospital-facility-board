c86c37a11b00a7032e898c09c2c18756928cdf33
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-07-11 09:12:25 +0100

    styling adjustments

--- client/src/pages/HomePage/components/HomeServicesSection/HomeServicesSection.jsx
+++ client/src/pages/HomePage/components/HomeServicesSection/HomeServicesSection.jsx
@@ -1,9 +1,10 @@
 import "./HomeServicesSection.css";
-import { useQuery } from "@tanstack/react-query";
 import Line from "../../../../components/Line/Line";
 import Loading from "../../../../components/Loading/Loading";
 import ErrorComponent from "../../../../components/ErrorComponent/ErrorComponent";
 import HomeServiceCard from "../HomeServiceCard/HomeServiceCard";
+import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
 
 const HomeServicesSection = () => {
 	const fetchServices = async () => {
@@ -22,6 +23,30 @@ const HomeServicesSection = () => {
 	});
 	const servicesData = data?.data;
 
+	const [numberOfCards, setNumberOfCards] = useState(null);
+
+	useEffect(() => {
+		// this is to set the number of Cards based on the window width
+		// i have tried to roughly estimate the transitions
+		const handleResize = () => {
+			if (window.innerWidth <= 600) {
+				setNumberOfCards(3);
+			} else if (window.innerWidth <= 1000) {
+				setNumberOfCards(4);
+			} else if (window.innerWidth <= 1200) {
+				setNumberOfCards(6);
+			}
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		handleResize();
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
 		<section className="home-services-section">
 			<h2 className="home-services-title">Our Services</h2>
@@ -30,7 +55,7 @@ const HomeServicesSection = () => {
 			{isError && <ErrorComponent error={error} />}
 			{!!servicesData && (
 				<div className="home-services-card-list">
-					{servicesData.map((service) => {
+					{servicesData.slice(0, numberOfCards).map((service) => {
 						const { slug, images, title, summary } = service;
 						return (
 							<HomeServiceCard
