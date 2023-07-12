61bf17f6572d5dfa615776c526e88d53313638fc
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-07-12 01:46:49 +0100

    implement mailchimp

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import "./Footer.css";
 import { NavLink } from "react-router-dom";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+
+const url =
+	"https://gmail.us21.list-manage.com/subscribe/post?u=062ecc3ec4949eb98966db14b&amp;id=0664e89465&amp;f_id=00da28e7f0";
 
 const Footer = () => {
 	const navLinks = [
@@ -13,8 +18,15 @@ const Footer = () => {
 		{ id: 8, to: "/contact-us", text: "Contact Us" },
 	];
 
-	const handleSubmit = (event) => {
+	const [email, setEmail] = useState(""); // Add state for the email input
+
+	const handleSubmit = (subscribe, event) => {
+		// Add 'subscribe' as a parameter
 		event.preventDefault();
+		const formData = new FormData(event.target);
+		const submittedEmail = formData.get("email");
+		subscribe({ EMAIL: submittedEmail });
+		setEmail(""); // Reset the email input
 	};
 
 	return (
@@ -62,12 +74,39 @@ const Footer = () => {
 				</div>
 				<div className="footer-subcontainer">
 					<h4 className="footer-subheading">SUBSCRIBE</h4>
-					<form onSubmit={handleSubmit} className="footer-subscribe-form">
-						<input type="text" className="footer-subscribe-form-input" />
-						<button type="submit" className="footer-subscribe-form-button">
-							Subscribe
-						</button>
-					</form>
+					<MailchimpSubscribe
+						url={url}
+						render={({ subscribe, status, message }) => (
+							<form
+								onSubmit={(event) => handleSubmit(subscribe, event)}
+								className="footer-subscribe-form"
+							>
+								<input
+									type="email"
+									name="email"
+									className="footer-subscribe-form-input"
+									placeholder="Your email"
+									value={email} // Set the value of the email input
+									onChange={(e) => setEmail(e.target.value)} // Handle input changes
+								/>
+								<button type="submit" className="footer-subscribe-form-button">
+									Subscribe
+								</button>
+								{status === "sending" && (
+									<div style={{ color: "blue" }}>sending...</div>
+								)}
+								{status === "error" && (
+									<div
+										style={{ color: "red" }}
+										dangerouslySetInnerHTML={{ __html: message }}
+									/>
+								)}
+								{status === "success" && (
+									<div style={{ color: "green" }}>Subscribed!</div>
+								)}
+							</form>
+						)}
+					/>
 				</div>
 			</div>
 		</footer>
