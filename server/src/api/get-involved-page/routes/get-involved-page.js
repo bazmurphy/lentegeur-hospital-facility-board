9261b62e490e1c6caae196346a410726d6d5cd12
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-07-12 21:03:53 +0100

    added about-page api route with model

---
+++ server/src/api/get-involved-page/routes/get-involved-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * get-involved-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::get-involved-page.get-involved-page');
---
+++ server/src/api/get-involved-page/services/get-involved-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * get-involved-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::get-involved-page.get-involved-page');
