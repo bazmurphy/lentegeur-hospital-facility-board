b50b9d848b81cf89f40f4f2e8e6ad72146c5427b
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-07-13 01:26:59 +0100

    implement gallery

--- client/src/pages/GalleryAlbumPage/GalleryAlbumPage.jsx
+++ client/src/pages/GalleryAlbumPage/GalleryAlbumPage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import "./GalleryAlbumPage.css";
 import { useParams, Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
@@ -5,6 +7,8 @@ import LoadingPage from "../LoadingPage/LoadingPage";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import Line from "../../components/Line/Line";
 import queryFetch from "../../utils/queryFetch";
+import "photoswipe/dist/photoswipe.css";
+import { Gallery, Item } from "react-photoswipe-gallery";
 
 const GalleryAlbumPage = () => {
 	const { slug } = useParams();
@@ -30,18 +34,32 @@ const GalleryAlbumPage = () => {
 						<h2 className="gallery-album-title">{galleryAlbumData.title}</h2>
 						<p className="gallery-album-summary">{galleryAlbumData.summary}</p>
 						<div className="gallery-album-list">
-							{galleryAlbumData.images.map((image) => {
-								const { id, url, alternativeText } = image;
-								return (
-									<div key={id} className="gallery-album-image-container">
-										<img
-											src={url}
-											alt={alternativeText}
-											className="gallery-album-image"
-										/>
-									</div>
-								);
-							})}
+							<Gallery>
+								<div className="gallery-container-gallery">
+									{galleryAlbumData.images.map((image) => {
+										const { id, url, alternativeText } = image;
+										return (
+											<Item
+												key={id}
+												original={url}
+												thumbnail={url}
+												width="1024"
+												height="768"
+											>
+												{({ ref, open }) => (
+													<img
+														ref={ref}
+														onClick={open}
+														src={url}
+														alt={alternativeText}
+														className="gallery-album-image"
+													/>
+												)}
+											</Item>
+										);
+									})}
+								</div>
+							</Gallery>
 						</div>
 					</div>
 				</div>
